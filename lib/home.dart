7c2aedf40f05e0534216f8a3595e858293d32ee9
leonblumenthal <dev.blumenthal.leon@gmail.com>
2019-12-26 00:11:45 +0100

    added courses list

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:questions/course.dart';
 import 'package:questions/models.dart';
+import 'package:questions/storage.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List<Course>> courses = Storage.getCourses();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,11 +19,29 @@ class Home extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () => Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => CourseWidget(Course())),
-            ),
+            onPressed: () async {
+              await Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => CourseWidget(Course())),
+              );
+              // Reload courses list.
+              courses = Storage.getCourses();
+              setState(() {} );
+            },
           )
         ],
+      ),
+      body: FutureBuilder(
+        future: courses,
+        builder: (BuildContext context, AsyncSnapshot<List<Course>> snapshot) {
+          if (snapshot.hasData) {
+            List<Course> courses = snapshot.data;
+            return ListView.builder(
+              itemBuilder: (_, i) => ListTile(title: Text(courses[i].title)),
+              itemCount: courses.length,
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
