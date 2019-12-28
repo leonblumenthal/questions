e56a6ede29ddcc89516b9464495bdfcc3bad81f5
leonblumenthal <dev.blumenthal.leon@gmail.com>
2019-12-28 20:35:19 +0100

    added orientation restriction

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:questions/home.dart';
 import 'package:questions/storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   await Storage.init();
   runApp(
     MaterialApp(
@@ -12,5 +17,3 @@ void main() async {
     ),
   );
 }
-
-
