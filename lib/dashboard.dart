f116548d60de45498b6562f7a4bff5ff0bee63e9
leonblumenthal <dev.blumenthal.leon@gmail.com>
2020-03-02 23:04:11 +0100

    improved dashboard style

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -23,26 +23,50 @@ class _DashboardState extends State<Dashboard> {
           )
         ],
       ),
-      body: FutureBuilder(
-          future: coursesFuture,
-          builder: (_, AsyncSnapshot<List<Course>> snapshot) {
-            if (snapshot.hasData) {
-              List<Course> courses = snapshot.data;
-              return ListView.builder(
-                itemBuilder: (context, int i) => ListTile(
-                  title: Text(courses[i].title),
-                  onTap: () => goToCourse(courses[i]),
-                ),
-                itemCount: courses.length,
-              );
-            }
-            return CircularProgressIndicator();
-          }),
+      body: FutureBuilder<List<Course>>(
+        future: coursesFuture,
+        builder: (_, snapshot) {
+          if (snapshot.hasData) return buildCourseList(snapshot.data);
+          return CircularProgressIndicator();
+        },
+      ),
     );
   }
 
-  /// Navigate to the course widget and
-  /// reload the courses after returning from it.
+  Widget buildCourseList(List<Course> courses) => ListView.builder(
+        padding: const EdgeInsets.all(4),
+        itemBuilder: (context, i) => buildCourseWidget(courses[i]),
+        itemCount: courses.length,
+      );
+
+  Widget buildCourseWidget(Course course) => Card(
+        child: InkWell(
+          borderRadius: BorderRadius.circular(4),
+          onTap: () => goToCourse(course),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  course.title,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                RaisedButton(
+                  child: Text('Answer'),
+                  color: Colors.tealAccent.shade200,
+                  colorBrightness: Brightness.dark,
+                  onPressed: null,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(64),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+
   Future goToCourse(Course course) async {
     await Navigator.of(context).push(
       MaterialPageRoute(builder: (context) => CourseWidget(course)),
